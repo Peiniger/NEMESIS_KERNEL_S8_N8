3c1ffb53da8fc94d2f99efc650bfe82d42453e4a
geiti94 <geiti94@me.com>
2017-09-17 16:03:55 -0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 13 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#13 SMP PREEMPT Sun Sep 17 12:36:55 PDT 2017"
+#define LINUX_COMPILE_BY "geiti94"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 6.4.0 (crosstool-NG crosstool-ng-1.23.0-217-gaacd6f3 - GCC 6.4.0 / Linux 4.13.2 by djb77) "
